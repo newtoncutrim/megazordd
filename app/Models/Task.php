718334110e7fc45bec9833c5098f78ecb0658937
newtoncutrim <newtoncutrim777@gmail.com>
2023-08-28 23:41:00 -0300

    teste

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public function user(){
+       return $this->belongsTo(User::class);
+    }
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'user_id'
+    ];
+    use HasFactory;
+}
