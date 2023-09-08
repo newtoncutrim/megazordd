f6527b0bd51c2f89516f8375267626d1a4fcfc44
newtoncutrim <newtoncutrim777@gmail.com>
2023-09-08 20:08:19 -0300

    ajuste

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        dd('redirect');
+        return $request->expectsJson() ? null : route('user.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,29 @@ use App\Http\Controllers\Admin\TaskController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('tasks/create', [TaskController::class, 'new'])->name('tasks.new');
 Route::post('tasks/new', [TaskController::class, 'create'])->name('tasks.create');
 Route::get('tasks/edit/{id}', [TaskController::class, 'edit'])->name('tasks.edit');
 Route::put('tasks/edit/{id}', [TaskController::class, 'update'])->name('tasks.update');
-
 Route::get('tasks/delete/{id}', [TaskController::class, 'detalhe'])->name('tasks.detalhe');
-//Route::resource('/tasks','App\\Http\\Controllers\\TasksController')->middleware(['auth']);
 Route::delete('tasks/delete/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
-
-
-Route::get('user/register', [UserController::class, 'register'])->name('user.register');
-
 Route::post('user/signup', [UserController::class, 'signup'])->name('user.signup');
-
-Route::get('user/login', [UserController::class, 'login'])->name('user.login');
-
 /* Route::get('user/sigin', [UserController::class, 'sigin'])->name('user.sigin'); */
-
 Route::post('user/auth', [UserController::class, 'auth'])->name('user.auth');
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
+
+
+
+
+
+Route::get('user/login', [UserController::class, 'login'])->name('user.login');
+Route::get('user/register', [UserController::class, 'register'])->name('user.register');
+
+Route::get('/', function () {
+    return view('user.login');
+});
 
 Route::fallback(function(){
     return view('404');
