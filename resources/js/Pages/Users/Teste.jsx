13da24aad946de18d436684043c25726d42a60e1
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-09-19 22:49:21 -0300

    Lógica do container / Alteração estilo css / import lib para ícones e fontes

--- resources/js/Pages/Users/Teste.jsx
+++ resources/js/Pages/Users/Teste.jsx
@@ -1,86 +1,95 @@
 import React from "react";
 import "./Teste.css";
-
-// const signUpButton = document.getElementById("signUp");
-// const signInButton = document.getElementById("signIn");
-// const container = document.getElementById("container");
-
-// signUpButton.addEventListener("click", () => {
-//     container.classList.add("right-panel-active");
-// });
-
-// signInButton.addEventListener("click", () => {
-//     container.classList.remove("right-panel-active");
-// });
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebookF,
+    faGooglePlusG,
+    faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Teste = () => {
-    function handleClick(event) {
-        console.log(event.target);
-    }
+    const handleSignUpClick = () => {
+        container.classList.add("right-panel-active");
+    };
+
+    const handleSignInClick = () => {
+        container.classList.remove("right-panel-active");
+    };
+
     return (
         <div>
-            <div class="container" id="container">
-                <div class="form-container sign-up-container">
+            <div className="container" id="container">
+                <div className="form-container sign-up-container">
                     <form action="#">
                         <h1>Criar uma conta</h1>
-                        <div class="social-container">
-                            <a href="#" class="social">
-                                <i class="fab fa-facebook-f"></i>
+                        <div className="social-container">
+                            <a href="#" className="social">
+                                <FontAwesomeIcon icon={faFacebookF} />
                             </a>
-                            <a href="#" class="social">
-                                <i class="fab fa-google-plus-g"></i>
+                            <a href="#" className="social">
+                                <FontAwesomeIcon icon={faGooglePlusG} />
                             </a>
-                            <a href="#" class="social">
-                                <i class="fab fa-linkedin-in"></i>
+                            <a href="#" className="social">
+                                <FontAwesomeIcon icon={faLinkedinIn} />
                             </a>
                         </div>
                         <span>ou use seu e-mail para cadastro</span>
                         <input type="text" placeholder="Name" />
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
-                        <button>Inscrever-se</button>
+                        <button onClick={handleSignInClick}>
+                            Inscrever-se
+                        </button>
                     </form>
                 </div>
-                <div class="form-container sign-in-container">
+                <div className="form-container sign-in-container">
                     <form action="#">
                         <h1>Entrar</h1>
-                        <div class="social-container">
-                            <a href="#" class="social">
-                                <i class="fab fa-facebook-f"></i>
+                        <div className="social-container">
+                            <a href="#" className="social">
+                                <FontAwesomeIcon icon={faFacebookF} />
                             </a>
-                            <a href="#" class="social">
-                                <i class="fab fa-google-plus-g"></i>
+                            <a href="#" className="social">
+                                <FontAwesomeIcon icon={faGooglePlusG} />
                             </a>
-                            <a href="#" class="social">
-                                <i class="fab fa-linkedin-in"></i>
+                            <a href="#" className="social">
+                                <FontAwesomeIcon icon={faLinkedinIn} />
                             </a>
                         </div>
                         <span>ou use sua conta</span>
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
                         <a href="#">Esqueceu sua senha?</a>
-                        <button onClick={handleClick}>Entrar</button>
+                        <button onClick={handleSignUpClick}>Entrar</button>
                     </form>
                 </div>
-                <div class="overlay-container">
-                    <div class="overlay">
-                        <div class="overlay-panel overlay-left">
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
                             <h1>Bem vindo de volta!</h1>
                             <p>
                                 Para se manter conectado conosco, faça login com
                                 suas informações pessoais
                             </p>
-                            <button class="ghost" id="signIn">
+                            <button
+                                onClick={handleSignInClick}
+                                className="ghost"
+                                id="signIn"
+                            >
                                 Entrar
                             </button>
                         </div>
-                        <div class="overlay-panel overlay-right">
+                        <div className="overlay-panel overlay-right">
                             <h1>Olá amigo!</h1>
                             <p>
                                 Insira seus dados pessoais e comece sua jornada
                                 conosco
                             </p>
-                            <button class="ghost" id="signUp">
+                            <button
+                                onClick={handleSignUpClick}
+                                className="ghost"
+                                id="signUp"
+                            >
                                 Inscrever-se
                             </button>
                         </div>
