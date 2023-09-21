900fbb5668e4593feb9f9fae80e1f38f2cd93f41
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-09-21 00:15:21 -0300

    Criação layout to-do-list

---
+++ resources/js/Pages/todo/main/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
+import React from "react";
+
+const App = () => (
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
+);
+
+export default App;
