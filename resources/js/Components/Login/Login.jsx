ff7ef2360eb629bc4348c9e5cbac60a67c0eafe0
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-10-21 17:03:54 -0300

    estilo pagina de login

--- resources/js/Components/Login/Login.jsx
+++ resources/js/Components/Login/Login.jsx
@@ -4,17 +4,26 @@ import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
+import styles from "../../../css/Login.module.css";
 
 const Login = () => {
     return (
-        <div>
-            <Routes>
-                <Route path="/" element={<LoginForm />}></Route>
-                <Route path="/criar" element={<LoginCreate />}></Route>
-                <Route path="/perdeu" element={<LoginPasswordLost />}></Route>
-                <Route path="/resetar" element={<LoginPasswordReset />}></Route>
-            </Routes>
-        </div>
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path="/" element={<LoginForm />}></Route>
+                    <Route path="/criar" element={<LoginCreate />}></Route>
+                    <Route
+                        path="/perdeu"
+                        element={<LoginPasswordLost />}
+                    ></Route>
+                    <Route
+                        path="/resetar"
+                        element={<LoginPasswordReset />}
+                    ></Route>
+                </Routes>
+            </div>
+        </section>
     );
 };
 
