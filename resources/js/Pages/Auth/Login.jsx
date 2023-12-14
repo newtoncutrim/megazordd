a15153a00da2dbcd7e83514d1e755f6e3846598c
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-12-13 23:25:34 -0300

    fix: estilos inciais pagina de login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -3,13 +3,35 @@ import styles from "./Login.module.css";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import { Link } from "@inertiajs/react";
+import Input from "@/Components/Forms/Input";
+import Button from "@/Components/Forms/Button";
 
 const Login = () => {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
         <div>
             <Header />
             <section className={styles.login}>
                 <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        type="text"
+                        onChange={({ target }) => setUsername(target.value)}
+                        value={username}
+                    />
+                    <Input
+                        type="password"
+                        onChange={({ target }) => setPassword(target.value)}
+                        value={password}
+                    />
+                    <Button>Entrar</Button>
+                </form>
                 <Link href="/cadastro ">Login Create</Link>
             </section>
             <Footer />
