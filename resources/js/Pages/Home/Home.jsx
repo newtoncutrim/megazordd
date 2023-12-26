fabe941a94e695d37a0c0ac8e066da0243e3d12e
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-12-26 19:00:38 -0300

    fix: adicionei uma imagem à home

--- resources/js/Pages/Home/Home.jsx
+++ resources/js/Pages/Home/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./Home.module.css";
+import Image from '../../../img/foto-home.jpg'
 
 const Home = () => {
     return (
         <div>
             <section className={`container ${styles.home}`}>
+                <div className={styles.homeContent}>
                 <h1 className={styles.titleHome}>
                     TaskHub<span className={styles.span}>.</span>
                 </h1>
                 <p className={styles.subTitleHome}>Gerencie suas tarefas </p>
+                </div>
+
+                <div className={styles.homeImage}>
+                    <img src={Image} alt="Imagem Home" />
+                </div>
             </section>
         </div>
     );
