d8e4d8e7731ede87cc989b427bbcc79d2128f56f
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-01-06 12:35:19 -0300

    fix: alteração component TodoForm

--- resources/js/Pages/Tasks/Todo/TodoForm.jsx
+++ resources/js/Pages/Tasks/Todo/TodoForm.jsx
@@ -1,66 +1,69 @@
-import { useState } from "react";
-import styles from './TodoForm.module.css'
+import React from "react";
 import axios from "axios";
+import styles from './TodoForm.module.css'
 
 const TodoForm = ({ addTodo }) => {
-  const [value, setValue] = useState("");
-  // const [category, setCategory] = useState("");
-  const [description, setDescription] = useState('')
+  const [value, setValue] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
-  // função para conectar formulário
   const handleSubmit = async (e) => {
-    e.preventDefault();
+      e.preventDefault();
 
-    const token = localStorage.getItem('token')
-    if(token){
-      try{
-        const response = await axios.post('http://localhost:8989/api/tasks', {title: value, description: description}, {
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-        } );
-        console.log('Tarefa criada com sucesso', response.data)
-      } catch (error){
-        console.log('Error ao criar tarefa', error)
+      const token = localStorage.getItem("token");
+
+      if (token) {
+          try {
+              setLoading(true);
+              const response = await axios.post(
+                  "http://localhost:8989/api/tasks",
+                  { title: value, description: description },
+                  {
+                      headers: {
+                          Authorization: `Bearer ${token}`,
+                      },
+                  }
+              );
+              console.log("Tarefa criada com sucesso", response.data);
+              addTodo(value, description);
+              setValue("");
+              setDescription("");
+          } catch(error) {
+             console.log('Erro ao criar tarefa', error)
+          } finally {
+              setLoading(false);
+          }
       }
-    }
 
-    // validação para dados nulos
-    if (!value || !description) return;
-    // adiciona os todos
-    addTodo(value, description);
-    // limpa os campos
-    setValue("");
-    setDescription("");
+      if (!value || !description) {
+          console.log("Por favor, preencha todos os campos.");
+          return;
+      }
   };
 
   return (
-    <div className={styles.TodoForm}>
-      <h2>Criar Tarefa:</h2>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-        className={styles.input}
-          type="text"
-          placeholder="Digite um Título"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <input
-        className={styles.input}
-          type="text"
-          placeholder="Faça uma descrição da tarefa"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        {/* <select className={styles.select} value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="">Selecione uma Categoria</option>
-          <option value="Trabalho">Trabalho</option>
-          <option value="Pessoal">Pessoal</option>
-          <option value="Estudos">Estudos</option>
-        </select> */}
-        <button className={styles.buttonTodo}>Criar Tarefa</button>
-      </form>
-    </div>
+      <div className={styles.TodoForm}>
+          <h2>Criar Tarefa:</h2>
+          <form onSubmit={handleSubmit} className={styles.form}>
+              <input
+                  className={styles.input}
+                  type="text"
+                  placeholder="Digite um Título"
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+              />
+              <input
+                  className={styles.input}
+                  type="text"
+                  placeholder="Faça uma descrição da tarefa"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+              />
+              <button className={styles.buttonTodo} disabled={loading}>
+                  {loading ? "Aguarde..." : "Criar Tarefa"}
+              </button>
+          </form>
+      </div>
   );
 };
 
